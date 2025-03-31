7853a99ebc8bb6eb0110b507af3f72a967de33aa
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2025-03-31 17:59:02 -0500

    feat: initial base of the application

---
+++ src/main/java/fast_order/FastOrderApplication.java
@@ -0,0 +1,13 @@
+package fast_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fast_order/FastOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package fast_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
