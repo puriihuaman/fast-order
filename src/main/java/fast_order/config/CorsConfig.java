42dfff8faf5f6e8f9419b93806b17dedc5140060
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2025-04-08 11:48:04 -0500

    config: add cors configuration

--- src/main/java/fast_order/config/CorsConfig.java
+++ src/main/java/fast_order/config/CorsConfig.java
@@ -1,4 +1,20 @@
 package fast_order.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS",
+            "HEAD",
+            "PATCH"
+        );
+    }
 }
