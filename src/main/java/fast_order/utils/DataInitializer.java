31366c7809b6c6cd52b94f82c6be12e33daf146e
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2025-04-08 11:49:55 -0500

    feat: add initializer data for the admin user

--- src/main/java/fast_order/utils/DataInitializer.java
+++ src/main/java/fast_order/utils/DataInitializer.java
@@ -1,13 +1,13 @@
-package fast_order.security;
+package fast_order.utils;
 
 import fast_order.dto.RoleTO;
 import fast_order.dto.UserTO;
 import fast_order.enums.RoleType;
-import fast_order.mapper.RoleMapper;
 import fast_order.service.RoleService;
 import fast_order.service.UserService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.dao.DataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,36 +18,36 @@ import java.util.Arrays;
 public class DataInitializer {
     private final UserService userService;
     private final RoleService roleService;
-    private final RoleMapper roleMapper;
+    private final PasswordEncoder passwordEncoder;
     
-    public DataInitializer(UserService userService, RoleService roleService, RoleMapper roleMapper)
+    public DataInitializer(
+        UserService userService,
+        RoleService roleService,
+        PasswordEncoder passwordEncoder
+    )
     {
         this.userService = userService;
         this.roleService = roleService;
-        this.roleMapper = roleMapper;
+        this.passwordEncoder = passwordEncoder;
     }
     
     @PostConstruct
     @Transactional
     public void init() {
         this.createRoles();
-        //        this.createAdminUser();
+        this.createAdminUser();
     }
     
     private void createRoles() {
         Arrays.stream(RoleType.values()).forEach((role) -> {
-            RoleTO roleDTO = RoleTO
+            RoleTO roleToCreate = RoleTO
                 .builder()
                 .roleName(role)
-                .description("Role " + role.name())
+                .description("Role " +
+                                 role.getRoleName())
                 .build();
             
-            RoleTO created = roleService.createRole(roleDTO);
-            System.out.println("************* ROL ************");
-            System.out.println(created.getId());
-            System.out.println(created.getRoleName());
-            System.out.println(created.getDescription());
-            System.out.println("************* ROL ************");
+            roleService.createRole(roleToCreate);
         });
     }
     
@@ -56,13 +56,7 @@ public class DataInitializer {
             RoleTO
                 role =
                 RoleTO.builder().roleName(RoleType.ADMIN).description("Role admin").build();
-            RoleTO adminRole = roleService.createRole(role);
-            System.out.println("********************************");
-            System.out.println("********** Rol creado");
-            System.out.println(adminRole.getId());
-            System.out.println(adminRole.getRoleName());
-            System.out.println(adminRole.getDescription());
-            System.out.println("********************************");
+            RoleTO createdRole = roleService.createRole(role);
             
             final String ENCODED_PASSWORD = "AdM¡N&20_25";
             
@@ -70,13 +64,17 @@ public class DataInitializer {
                 .builder()
                 .name("Bytes Colaborativos")
                 .email("bytes@colaborativos.es")
-                .password(ENCODED_PASSWORD)
+                .password(passwordEncoder.encode(ENCODED_PASSWORD))
                 .signUpDate(LocalDate.now())
                 .totalSpent(0.0)
-                .roleId(adminRole.getId())
+                .roleId(createdRole.getId())
                 .build();
-            System.out.println(user.getRoleId());
-            userService.createUser(user);
+            
+            UserTO userCreated = userService.createUser(user);
+            System.out.println("--------- USER CREATED ----------");
+            System.out.println(userCreated.getId());
+            System.out.println(userCreated.getEmail());
+            System.out.println("--------- USER CREATED ----------");
         } catch (DataAccessException ex) {
             throw new RuntimeException("Error database accesses");
         } catch (Exception ex) {
